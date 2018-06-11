c53d242fa7c397f2656cd7929676a1baa6f21b73
SpQuyt <spquyt@gmail.com>
2018-06-11 16:59:25 +0700

    hmmm done a website

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
 function change(){
-			var inString = document.getElementById("InputString").value;
-			var len = inString.length;
-			var outstring = document.getElementById("demo");
-			var count1 = 0;
-			var count2 = 0;
-			outstring.innerHTML = '';
+	var inString = document.getElementById("InputString").value;
+	var len = inString.length;
+	var outstring = document.getElementById("OutputString");
+	var count1 = 0;
+	var count2 = 0;
+	outstring.innerHTML = '';
 
-			for (var i = 0; i < len; i++){
-				var random = Math.floor((Math.random() * 2) + 1);
-				if (random == 1){
-					count1++;
-					if (count1 < 3){
-						outstring.innerHTML += inString[i].toLowerCase();
-					}
-					else {
-						outstring.innerHTML += inString[i].toUpperCase();
-						count1 = 0;
-						count2++;
-					}
-				}
-				else if (random == 2){
-					count2++;
-					if (count2 < 3){
-						outstring.innerHTML += inString[i].toUpperCase();
-					}
-					else {
-						outstring.innerHTML += inString[i].toLowerCase();
-						count2 = 0;
-						count1++;
-					}	
-				}
+	for (var i = 0; i < len; i++){
+		var random = Math.floor((Math.random() * 2) + 1);
+		if (random == 1){
+			count1++;
+			if (count1 < 3){
+				outstring.innerHTML += inString[i].toLowerCase();
+			}
+			else {
+				outstring.innerHTML += inString[i].toUpperCase();
+				count1 = 0;
+				count2++;
 			}
 		}
+		else if (random == 2){
+			count2++;
+			if (count2 < 3){
+				outstring.innerHTML += inString[i].toUpperCase();
+			}
+			else {
+				outstring.innerHTML += inString[i].toLowerCase();
+				count2 = 0;
+				count1++;
+			}	
+		}
+	}
+}
 
-		document.addEventListener("keyup", (event) => {
-		    switch (event.keyCode) {
-		    	case 13:
-		    		change();
-		    }
-		});
+function wannacopy(){
+	var text = document.querySelector("#OutputString");
+	text.select();
+	document.execCommand("copy");
+}
+
+document.addEventListener("keyup", (event) => {
+    switch (event.keyCode) {
+    	case 13:
+    		change();
+    }
+});
