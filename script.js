e0306569959f77dd6ddd82a733b634d2414130b5
Zhang UK <spquyt@gmail.com>
2018-06-20 00:37:51 +0700

    .

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+var counter = 0;
+function color_fking_change(counter) {
+	if (counter == 10){
+		counter = 0;
+	}
+	else{
+		colorchange1 = setTimeout(
+		function() {
+			document.getElementById("quoc").style.color = "red";
+		},1000);
+	colorchange2 = setTimeout(
+		function() {
+			document.getElementById("quoc").style.color = "black";
+		},1800);
+	colorchange3 = setTimeout(
+		function() {
+			document.getElementById("quoc").style.color = "yellow";
+		},2600);
+	colorchange4 = setTimeout(
+		function() {
+			document.getElementById("quoc").style.color = "green";
+		},3400);
+	
+	}
+	color_fking_change(counter+1);
+}
+	
+	// clearTimeout(colorchange1);
+	// clearTimeout(colorchange2);
+	// clearTimeout(colorchange3);
+	// clearTimeout(colorchange4);
+
+setInterval(color_fking_change(),0);
 function change(){
 	var inString = document.getElementById("InputString").value;
 	var len = inString.length;
@@ -5,6 +38,8 @@ function change(){
 	var count1 = 0;
 	var count2 = 0;
 	outstring.innerHTML = '';
+
+
 
 	for (var i = 0; i < len; i++){
 		var random = Math.floor((Math.random() * 2) + 1);
